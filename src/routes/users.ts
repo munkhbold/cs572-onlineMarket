fd6c0cc549f2bb62ac105716c1c131a9e51c69da
billywap <64044281+billywap@users.noreply.github.com>
2020-06-13 23:53:22 -0500

    closes #5

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { login, register, updateCart} from '../controllers/user';
+import { login, register, updateCart, getCart} from '../controllers/user';
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
 
 router.post('/shopping-cart/update', updateCart);
+router.get('/shopping-cart', getCart);
 
 export default router;
