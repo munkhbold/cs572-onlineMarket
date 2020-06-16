fec75b0098653f71a29880c889e9ce74431f33de
Munkhbold <munkhbold.d@nmtec.co>
2020-06-16 12:30:53 -0500

    close #26

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import users from './users';
 import orders from './orders';
 import products from './products';
+import auth from './auth';
 import { Router } from 'express';
 
 const router = Router();
 
+router.use('/', auth);
 router.use('/', users);
 router.use('/orders', orders);
 router.use('/products', products);
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -9,17 +9,18 @@ import {
     updateProduct,
     removeProductById
 } from '../controllers/product';
+import { verifyAdmin, verifyBuyer, verifySeller } from '../middlewares/verifyTokenMiddleware';
 
 const router = Router();
 
-router.put('/:productId/approve', approveProduct)
-router.post('/:productId/reviews', addReview)
-router.put('/:productId/reviews/:reviewId/approve', approveReview)
+router.put('/:productId/approve', verifyAdmin, approveProduct)
+router.post('/:productId/reviews', verifyBuyer, addReview)
+router.put('/:productId/reviews/:reviewId/approve', verifyAdmin, approveReview)
 
 router.get('/', getProducts);
-router.post('/', createProduct);
+router.post('/', verifySeller, createProduct);
 router.get('/:productId', getProductById);
-router.put('/:productId', updateProduct);
-router.delete('/:productId', removeProductById);
+router.put('/:productId', verifySeller, updateProduct);
+router.delete('/:productId', verifySeller, removeProductById);
 
 export default router;
