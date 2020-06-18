2834bb39199cc2dfd7a1e262ff56a1d6ccb8b740
billywap <64044281+billywap@users.noreply.github.com>
2020-06-17 20:03:50 -0500

    error handler cart request

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,24 @@
 import {ApiResponse} from '../utils/response';
 
 export const updateCart = async (req, res, next) => {
-  const user = req.user;
-  const productId = req.body.productId;
-  const quantity = req.body.quantity;
-  await user.updateCart(productId, quantity);
-  res.status(200).send(new ApiResponse(200, 'success', {user: user}));
+  try {
+    const user = req.user;
+    const productId = req.body.productId;
+    const quantity = req.body.quantity;
+    await user.updateCart(productId, quantity);
+    res.status(200).send(new ApiResponse(200, 'success', {user: user}));
+  }
+  catch (err) {
+    res.status(401).send(new ApiResponse(200, 'error', { errors: [err.message]}));
+  }
 }
 
 export const getCart = async (req, res, next) => {
-  const user = req.user;
-  res.status(200).send(new ApiResponse(200, 'success', {cart: user.cart}));
+  try {
+    const user = req.user;
+    res.status(200).send(new ApiResponse(200, 'success', {cart: user.cart}));
+  }
+  catch (err) {
+    res.status(401).send(new ApiResponse(200, 'error', { errors: [err.message]}));
+  }
 }
