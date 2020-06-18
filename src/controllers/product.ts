62749b1bd1c4d1a97e229eb5ee754fd27f52752b
billywap <64044281+billywap@users.noreply.github.com>
2020-06-18 18:41:07 -0500

    changed update in prod

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -21,12 +21,10 @@ export const updateProduct = async (req, res, next) => {
   try {  
     const product = await Product.update({_id: req.params.productId} , {
       name: req.body.name,
-      category: req.body.category,
       sellerId: req.user._id,
       description: req.body.description,
       imageUrl: req.body.imageUrl,
-      unitPrice: req.body.unitPrice,
-      qty: req.body.qty
+      unitPrice: req.body.unitPrice
     });
     res.status(200).send(new ApiResponse(200, 'success', {product}));
   } catch (e) {
